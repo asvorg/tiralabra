b5b153e1e1ed4018c6b1a12c752430e162488bb1
unknown <aapo_koskinen@hotmail.com>
2023-06-02 19:07:54 +0300

    added astrofloat

--- rsa/src/keygen.rs
+++ rsa/src/keygen.rs
@@ -1,6 +1,7 @@
 use num_bigfloat::BigFloat;
 use num_bigint::{BigUint, ToBigUint};
 use num_traits::{One, Zero,ToPrimitive};
+use astro_float::BigFloat as AstroBigFloat;
 use crate::prime_func::PrimeFunc;
 pub struct Keygen;
 impl Keygen{
@@ -62,11 +63,12 @@ pub fn keygen(num:u64) -> ((BigUint, BigUint), (BigUint, BigUint)){
 
     //calculate rsa d value
     pub fn calculate_d(e: BigUint, phi: BigUint) -> BigUint{
-        let e_float: BigFloat = e.to_f64().unwrap().into();
-        let mut phi_float: BigFloat = phi.to_f64().unwrap().into();
-        let phi_float_orig: BigFloat = phi_float;    
+        let e_float: AstroBigFloat = e.to_f64().unwrap().into();
+        let mut phi_float: AstroBigFloat = phi.to_f64().unwrap().into();
+        let phi_float_orig: AstroBigFloat = phi_float;   
+        let astro_one: AstroBigFloat =1.0.to_f64().unwrap().into();  
         
-        let mut d_float: BigFloat = (BigFloat::one() + phi_float) / e_float;
+        let mut d_float: AstroBigFloat = (astro_float::BigFloat::div(&astro_float::BigFloat::add(&phi_float, &astro_one, 2048, astro_float::RoundingMode::ToEven),&e_float, 2048, astro_float::RoundingMode::ToEven), &astro_one, 2048, astro_float::RoundingMode::ToEven);
 
         while (BigFloat::one() + phi_float) % e_float != BigFloat::zero(){
             //println!("d_float: {}", d_float);  
@@ -78,5 +80,6 @@ pub fn keygen(num:u64) -> ((BigUint, BigUint), (BigUint, BigUint)){
         d
   
     }
+
 }
     
